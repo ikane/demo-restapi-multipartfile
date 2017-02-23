80a2c1ad887081f566ad990fffb02f8a08d1e3d1
ikane <ibrahima.kane@verifone.com>
2017-02-23 13:20:01 +0100

    some refactorings

--- src/main/java/com/example/Initializer.java
+++ src/main/java/com/example/Initializer.java
@@ -2,10 +2,13 @@ package com.example;
 
 import java.lang.invoke.MethodHandles;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -14,7 +17,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+//@Component
 public class Initializer implements CommandLineRunner {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -36,7 +39,8 @@ public class Initializer implements CommandLineRunner {
 		
 		
 		MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();           
-		parts.add("file", new FileSystemResource(resource.getFile()));
+//		parts.add("file", new FileSystemResource(resource.getFile()));
+		parts.add("file", new ByteArrayResource(IOUtils.toByteArray(resource.getInputStream())));
 		
 		String response = this.restTemplate.postForObject("http://localhost:8080/api/upload", parts, String.class);
 
--- src/test/java/com/example/RestApiTest.java
+++ src/test/java/com/example/RestApiTest.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import java.io.FileInputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.entity.mime.content.FileBody;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
@@ -42,9 +50,27 @@ public class RestApiTest {
 		
 		
 		MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();           
-		parts.add("file", new FileSystemResource(resource.getFile()));
+//		parts.add("file", new FileSystemResource(resource.getFile()));
+//		parts.add("file", new ByteArrayResource(IOUtils.toByteArray(resource.getInputStream())));
+		parts.add("file", new FileInputStream(resource.getFile()));
 		
-		String response = this.restTemplate.postForObject("/api/upload", parts, String.class);
+		FileBody uploadFilePart = new FileBody(resource.getFile());
+		byte[] byteArray = IOUtils.toByteArray(resource.getInputStream());
+		ByteArrayResource byteArrayResource = new ByteArrayResource(byteArray);
+		
+		HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(new MediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+         
+        
+				
+//        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+        HttpEntity<byte[]> requestEntity = new HttpEntity<>(byteArray, headers);
+//        HttpEntity<ByteArrayResource> requestEntity = new HttpEntity<>(byteArrayResource, headers);
+        //HttpEntity<ByteArrayResource> requestEntity = new HttpEntity<>(byteArrayResource, headers);
+		
+//		String response = this.restTemplate.postForObject("/api/upload", parts, String.class);
+		String response = this.restTemplate.postForObject("/api/upload", requestEntity, String.class);
 		
 		//ResponseEntity<String> response = this.restTemplate.getForEntity("/hello", String.class);
 		
